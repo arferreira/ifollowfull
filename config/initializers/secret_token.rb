2e6ce865372610a10ed9dfb490bbaad3cfc19d83
Antonio Ricar MoSh <antonioricardo@ifollowagencia.com.br>
2013-10-09 14:25:21 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ifollow::Application.config.secret_token = '8c3e70226dae3fad0fea12d43e25c78c1a30c7e2aafa0412175b9abb51d00fce685ea040d3239e3a71b653c8d09ac92d18b3ea0cccb0e4bc9c5aaa957850a0b3'
